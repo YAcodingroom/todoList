06e79320d244d0f1e1e0af424b73337df4248339
YAcodingroom <n12a18a@gmail.com>
2024-03-19 11:00:22 +0800

    feat: add router & PageNotFound component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { TodoPage, LoginPage, SignUpPage, HomePage } from './pages';
 import './App.scss';
-import { TodoPage, LoginPage, SignUpPage } from './pages';
+import PageNotFound from 'pages/PageNotFound';
 
 function App() {
   return (
     <div className="app">
-      <TodoPage />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+          <Route path="todo" element={<TodoPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
