d20e7fb9c61331a78258dd8a3c344116e579c05b
YAcodingroom <n12a18a@gmail.com>
2024-03-21 21:42:17 +0800

    feat: add router link & props at login & signup page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,8 +6,13 @@ import {
 } from 'components/common/auth.styled';
 import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <AuthContainer>
       <div>
@@ -16,14 +21,27 @@ const LoginPage = () => {
       <h1>登入 Todo</h1>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="帳號"
+          placeholder="請輸入帳號"
+          value={username}
+          onChange={(accountValue) => setUsername(accountValue)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="密碼"
+          placeholder="請輸入密碼"
+          type="password"
+          value={password}
+          onChange={(passwordValue) => setPassword(passwordValue)}
+        />
       </AuthInputContainer>
       <AuthButton>登入</AuthButton>
-      <AuthLinkText>註冊</AuthLinkText>
+      <Link to="/signup">
+        <AuthLinkText>註冊</AuthLinkText>
+      </Link>
     </AuthContainer>
   );
 };
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -4,10 +4,16 @@ import {
   AuthButton,
   AuthLinkText,
 } from 'components/common/auth.styled';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
 
 const SignUpPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
   return (
     <AuthContainer>
       <div>
@@ -16,18 +22,36 @@ const SignUpPage = () => {
       <h1>建立您的帳號</h1>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="帳號"
+          placeholder="請輸入帳號"
+          value={username}
+          onChange={(accountValue) => setUsername(accountValue)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="Email"
+          placeholder="請輸入 email"
+          value={email}
+          onChange={(emailValue) => setEmail(emailValue)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="密碼"
+          placeholder="請輸入密碼"
+          type="password"
+          value={password}
+          onChange={(passwordValue) => setPassword(passwordValue)}
+        />
       </AuthInputContainer>
       <AuthButton>註冊</AuthButton>
-      <AuthLinkText>取消</AuthLinkText>
+      <Link to="/login">
+        <AuthLinkText>取消</AuthLinkText>
+      </Link>
     </AuthContainer>
   );
 };
