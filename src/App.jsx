9d7c0dfde50453789a0c0325b5db1ba0ef625878
YAcodingroom <n12a18a@gmail.com>
2024-03-22 17:46:38 +0800

    feat: add home page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { TodoPage, LoginPage, SignUpPage, HomePage } from './pages';
 import './App.scss';
-import PageNotFound from 'pages/PageNotFound';
 import { AuthProvider } from 'contexts/AuthContext';
 
 function App() {
@@ -10,11 +9,10 @@ function App() {
       <BrowserRouter>
         <AuthProvider>
           <Routes>
-            <Route index element={<HomePage />} />
+            <Route path="*" element={<HomePage />} />
             <Route path="login" element={<LoginPage />} />
             <Route path="signup" element={<SignUpPage />} />
             <Route path="todo" element={<TodoPage />} />
-            <Route path="*" element={<PageNotFound />} />
           </Routes>
         </AuthProvider>
       </BrowserRouter>
